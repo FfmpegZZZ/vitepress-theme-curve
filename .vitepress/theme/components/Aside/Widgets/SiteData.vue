c34ce6cce524fe30f855c5b1caf40e643ebc8c7b
FfmpegZZZ <yaobestlife@gmail.com>
2025-03-02 20:44:32 +0800

    remove js

--- .vitepress/theme/components/Aside/Widgets/SiteData.vue
+++ .vitepress/theme/components/Aside/Widgets/SiteData.vue
@@ -45,7 +45,7 @@ import { daysFromNow } from "@/utils/helper";
 const { theme } = useData();
 
 onMounted(() => {
-  loadScript("https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js", {
+  loadScript(".vitepress\theme\components\Aside\Widgets\busuanzi.pure.mini.js", {
     async: true,
     reload: true,
   });
