d68d2387232a877f46ed6fec8adb0f176f34aad6
Orwells <Orwells@noreply.gitcode.com>
2025-03-02 23:46:59 +0800

    添加评论

--- themeConfig.mjs
+++ themeConfig.mjs
@@ -276,7 +276,7 @@ export const themeConfig = {
     enable: false,
     // 评论系统选择
     // artalk / twikoo
-    type: "artalk",
+    type: "twikoo",
     // artalk
     // https://artalk.js.org/
     artalk: {
@@ -288,9 +288,9 @@ export const themeConfig = {
     twikoo: {
       // 必填，若不想使用 CDN，可以使用 pnpm add twikoo 安装并引入
       js: "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/twikoo/1.6.39/twikoo.all.min.js",
-      envId: "",
+      envId: "https://twikoo.696898.xyz",
       // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
-      region: "ap-shanghai",
+      region: "",
       lang: "zh-CN",
     },
   },
